24dce4c0b44f4f35aa7a0b96acc9eb66cf670ab0
2024ashikul <2024ashikul@gmail.com>
2025-11-14 18:54:21 +0600

    ADD

--- backend/src/archive/routes/codeSubmit.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const { codeSubmit, codeRun, codeRunTemp } = require('../controllers/codeSubmit')
-const router = express.Router()
-
-router.post('/runcode',codeRun)
-
-
-
-module.exports = router;
